64f1a1ec49563e4f35b33a81b15367e6157debfd
vagrant <vagrant@homestead>
2017-11-20 03:59:09 +0000

    add zuofei

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,25 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(GLPzmlTableSeeder::class);
         $this->call(GLPznrTableSeeder::class);
+
+        //删除作废凭证数据
+        $this->delete_zf_pz();
+
+
+    }
+
+    /**
+     *
+     * 删除作废凭证
+     *
+     */
+    
+
+    protected function delete_zf_pz()
+    {
+        $zuofei_pzs = \DB::table('GL_Pzml')->where('zt','0')->get(['kjqj','pzh']);
+        foreach ($zuofei_pzs as $zuofei_pz) {
+            \DB::table('GL_Pznr')->where('kjqj', $zuofei_pz->kjqj)->where('pzh', $zuofei_pz->pzh)->delete();
+        }
     }
 }
